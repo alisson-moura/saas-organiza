4cbed6358b0a1b4baa4f7c142b9276a65483b4aa
alisson-moura <alisson.mo.moura@outlook.com.br>
2024-12-06 15:09:47 -0300

    fix: teme padrão alterado para dark

--- apps/app/src/app.tsx
+++ apps/app/src/app.tsx
@@ -7,7 +7,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 
 export function App() {
   return (
-    <ThemeProvider defaultTheme="system" storageKey="organiza-theme">
+    <ThemeProvider defaultTheme="dark" storageKey="organiza-theme">
       <Toaster />
       <trpc.Provider client={trpcClient} queryClient={queryClient}>
         <QueryClientProvider client={queryClient}>
